5f5bd7c12ed924a757e9b80851a5a81f68347773
Chris Olande <olandechris@outlook.com>
2025-06-26 20:05:14 +0300

    Initializes the main application class
    Initializes the main application class with core components.
    
    This commit introduces the `Main` class, which serves as the central point for application initialization and orchestration. It sets up the language model, embedding model, knowledge graph, query engine, and visualizer, laying the foundation for processing documents and building a knowledge graph.

--- src/core/main.py
+++ src/core/main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from finalrag!")
+import os
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_openai import ChatOpenAI
+
+from src.knowledge_graph.knowledge_graph import KnowledgeGraph
+from src.nlp.rag_chain import QueryEngine
+
+# from visualization import Visualizer
+from src.nlp.vectorstore import VectorStore
 
 
-if __name__ == "__main__":
-    main()
+class Main:
+    def __init__(self):
+        self.llm = ChatOpenAI(
+            model="meta-llama/llama-3.3-70b-instruct",
+            api_key=os.getenv("OPENROUTER_API_KEY"),
+            openai_api_base="https://openrouter.ai/api/v1",
+            temperature=0,
+            streaming=False,
+        )
+
+        self.embedding_model = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+        self.knowledge_graph = KnowledgeGraph(cache_dir="../my_cache")
+        self.query_engine = None
+        # self.visualizer = Visualizer()?
+
+    def process_documents(self, documents):
+        self.vector_store = VectorStore()
+        self.knowledge_graph.build_knowledge_graph(documents, self.llm)
+        self.query_engine = QueryEngine(
+            self.vector_store, self.knowledge_graph, self.llm
+        )
